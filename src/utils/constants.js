ccb4af5a00b4023b0be5472f2c52ee713c750e79
AshnaMg <ashnamg1998@gmail.com>
2023-11-29 18:16:39 +0530

    2nd commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,10 +16,6 @@ export const logo = 'https://www.freeiconspng.com/thumbs/youtube-logo-png/hd-you
 
 export const categories = [
   { name: 'New', icon: <HomeIcon />, },
-  { name: 'JS Mastery', icon: <CodeIcon />, },
-  { name: 'Coding', icon: <CodeIcon />, },
-  { name: 'ReactJS', icon: <CodeIcon />, },
-  { name: 'NextJS', icon: <CodeIcon />, },
   { name: 'Music', icon: <MusicNoteIcon /> },
   { name: 'Education', icon: <SchoolIcon />, },
   { name: 'Podcast', icon: <GraphicEqIcon />, },
